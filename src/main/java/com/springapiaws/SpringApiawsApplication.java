4c068d06eb547fc0a21ab7f7db7e73c7d40f8eda
Lucas Diniz <53830752+Akssasori@users.noreply.github.com>
2021-06-04 07:37:56 -0300

    Initial commit

---
+++ src/main/java/com/springapiaws/SpringApiawsApplication.java
@@ -0,0 +1,13 @@
+package com.springapiaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiawsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springapiaws/SpringApiawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapiaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
